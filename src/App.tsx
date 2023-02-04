1b98eadf0e5829d883a62985fecc76a4cf7b3642
Ivan Kuznetsov <96410453+MightComeback@users.noreply.github.com>
2023-02-04 13:51:46 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  const [navActive, setNavActive] = useState(0);
+
+  return (
+    <>
+      <nav
+        className="flex flex-col float-left h-screen w-24
+         shadow-lg items-center p-2"
+      >
+        <ul className="[&_li]:border-b [&_li]:my-4 [&_li]:active:bg-red-100">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
