fe5360af351eabb34a920cdbd57228ddcf845214
Ivan Kuznetsov <96410453+MightComeback@users.noreply.github.com>
2023-02-10 11:07:02 +0200

    Task data manual load

--- client/src/pages/Todo.tsx
+++ client/src/pages/Todo.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TaskComponent from "../../components/TaskComponent";
-import Task from "../../model/Task";
+import Task from "../../../server/model/Task";
+
+type Task = {
+  title: string;
+};
 
 const Todo = () => {
-  const [taskData, setTaskData] = useState();
+  const [taskData, setTaskData] = useState([{}]);
+  let tasks: string[];
 
   const getTaskDataFromServer = () => {
     axios
@@ -13,14 +18,28 @@ const Todo = () => {
       .catch((err) => console.log(err));
   };
 
+  useEffect(() => {
+    taskData.map((task) => console.log(task["title"]));
+  }, [getTaskDataFromServer]);
+
   return (
     <div className="flex py-12 flex-col">
       <div className="mb-16 w-full flex justify-center">
         <h1 className="text-xl uppercase">Your tasks:</h1>
       </div>
       <section className="space-y-6 ml-24">
-        <button onClick={getTaskDataFromServer}>GetData</button>
-        <p>data: {taskData}</p>
+        <button
+          onClick={() => {
+            getTaskDataFromServer();
+          }}
+        >
+          GetData
+        </button>
+        <div>
+          {taskData.map((task, idx) => (
+            <p key={idx}>{task["title"]}</p>
+          ))}
+        </div>
       </section>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Task = require('../server/model/Task')
+require('dotenv').config()
 
 const app = express()
+const uri = process.env.DATABASE_URL;
 
-const connect = () => {
-  mongoose.connect("")
-}
+const connect = async () => {
+  await mongoose.connect(uri).then(() => console.log("Connected to db")).catch((err) => console.log(err))
+};
 
-app.get("/todo_api", (req, res) => {
-  res.json("tasks")
+connect();
+
+app.get("/todo_api", async (req, res) => {
+  try {
+    const data = await Task.find();
+    res.json(data)
+  } catch (err) {
+    res.send({ message: "Error fetching" });
+  }
 })
 
 app.listen(8000, function(err) {
