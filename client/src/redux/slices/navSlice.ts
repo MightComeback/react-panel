7a2ec1886104de7dacd57e943ad6542267dfeff0
Ivan Kuznetsov <96410453+MightComeback@users.noreply.github.com>
2023-02-11 11:22:58 +0200

    Uncommited

---
+++ client/src/redux/slices/navSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentNavPage: '',
+  isNavOpen: true,
+}
+
+export const navSlice = createSlice({
+  name: 'navStateManagement',
+  initialState,
+  reducers: {
+    setCurrentNavPage: (state, action) => {
+      state.currentNavPage = action.payload
+    },
+    setIsNavOpen: (state, action) => {
+      state.isNavOpen = action.payload
+    },
+  },
+})
+
+export const { setCurrentNavPage, setIsNavOpen } = navSlice.actions
+
+export default navSlice.reducer
---
+++ client/src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import navSliceReducer from './slices/navSlice';
+
+export const store = configureStore({
+  reducer: {
+    navStateManagement: navSliceReducer
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
